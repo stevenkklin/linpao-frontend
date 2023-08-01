13a6c4bbcabdf03960a77e459aba8b0e349de3c2
736327363@qq.com <736327346@qq.com>
2023-08-01 12:31:18 +0800

    feature: test

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -1,46 +1,32 @@
 <script lang="ts" setup>
-import {useRouter} from "vue-router";
+import { useRouter } from 'vue-router'
 
-const router = useRouter();
+const router = useRouter()
 
 const onClickLeft = () => {
-  router.back();
-};
+    router.back()
+}
 const onClickRight = () => {
-  router.push('/search')
-};
-
+    router.push('/search')
+}
 
 const onChange = () => {}
-
 </script>
 
 <template>
-  <van-nav-bar
-      title="标题"
-      right-text="按钮"
-      left-arrow
-      @click-left="onClickLeft"
-      @click-right="onClickRight"
-  >
-    <template #right>
-      <van-icon name="search" size="18" />
-    </template>
-  </van-nav-bar>
-  <div id="content">
-    <router-view />
-  </div>
-  <van-tabbar route @change="onChange">
-    <van-tabbar-item replace to="/" icon="home-o" name="index">主页</van-tabbar-item>
-    <van-tabbar-item replace to="/model" icon="search" name="team">队伍</van-tabbar-item>
-    <van-tabbar-item replace to="/user" icon="friends-o" name="user">个人</van-tabbar-item>
-  </van-tabbar>
+    <van-nav-bar title="标题" righ t-text="按钮" left-arrow @click-left="onClickLeft" @click-right="onClickRight">
+        <template #right>
+            <van-icon name="search" size="18" />
+        </template>
+    </van-nav-bar>
+    <div id="content">
+        <router-view />
+    </div>
+    <van-tabbar route @change="onChange">
+        <van-tabbar-item replace to="/" icon="home-o" name="index">主页</van-tabbar-item>
+        <van-tabbar-item replace to="/model" icon="search" name="team">队伍</van-tabbar-item>
+        <van-tabbar-item replace to="/user" icon="friends-o" name="user">个人</van-tabbar-item>
+    </van-tabbar>
 </template>
 
-
-
-
-
-<style scoped>
-
-</style>
+<style scoped></style>
